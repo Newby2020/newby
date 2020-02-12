eb7ece9d4e27a8df8f91b0d5193770b0bce264c2
raejin <user2@DESKTOP-5KOT1D7>
2020-02-12 21:41:46 +0900

    어드민 커밋

--- src/com/kh/newby/admin/controller/ClassListServlet.java
+++ src/com/kh/newby/admin/controller/ClassListServlet.java
@@ -1,11 +1,16 @@
 package com.kh.newby.admin.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.newby.Class.model.vo.ClassVo;
+import com.kh.newby.admin.service.AdminService;
 
 /**
  * Servlet implementation class ClassApply
@@ -26,8 +31,24 @@ public class ClassListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 1. 페이징 처리하기
+		ArrayList<ClassVo> list = null;
+		AdminService as = new AdminService();
+		
+		int startPage;
+		int endPage;
+		int maxPage;
+		int currentPage;
+		int limit;
+		
+		currentPage = 1;
+		limit = 10;
+		
+		if(request.getParameter("currentPage") != null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		
+		
 	}
 
 	/**
