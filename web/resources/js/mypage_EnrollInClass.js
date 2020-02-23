ebf16bd9d80a7d53c04aca2b699ab6559240e9a6
jihoon <jihoon@DESKTOP-992GO2V>
2020-02-23 20:57:48 +0900

    자료 업데이트

--- web/resources/js/mypage_EnrollInClass.js
+++ web/resources/js/mypage_EnrollInClass.js
@@ -148,7 +148,7 @@ function addSchedule(){
 $(function(){
 	$(document).on('click','#ss',function(){
 		$('#div11')
-		.append('<div><input name="cdate" type="date" class="dataCl">&nbsp;&nbsp;<input name="stime" type="time" class="dataCl">&nbsp;&nbsp;<img id="delImg" class="item1" src="/semi/resources/images/deleteImg.png" alt="xIcon"></div>');
+		.append('<div><input name="cdate" type="date" class="dataCl">&nbsp;&nbsp;<input name="stime" type="time" class="dataCl">&nbsp;&nbsp;<img id="delImg" class="item1" src="/semi/resources/images/deleteImg.png" alt="xIcon"></div>');       
 	});
 	
 	$(document).on('click','.item1',function(){
